d1fe3ec69c46de336089fa990f012aeb4eaeca23
Emanuel Severino <emanuelsmseverino@gmail.com>
2024-02-16 11:11:16 -0300

    Migrations

--- produto/models.py
+++ produto/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome = models.CharField()
-    preco = models.DecimalField()
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(decimal_places=2, max_digits=8)
 
     class Meta:
         verbose_name = 'Produto'
